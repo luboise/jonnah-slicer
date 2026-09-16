60d25c342c9ec507331128602aa4d806cfd29001
luboise <lucas2@live.com.au>
2026-09-17 05:45:08 +1000

    App: Add Icon On Windows

---
+++ build.rs
@@ -0,0 +1,19 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(target_os = "windows")]
+    set_icon()?;
+
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+fn set_icon() -> Result<(), Box<dyn std::error::Error>> {
+    const ICON_PATH: &str = "assets/jonnah.ico";
+
+    let mut res = winresource::WindowsResource::new();
+    res.set_icon(ICON_PATH)
+        // manually set version 1.0.0.0
+        .set_version_info(winresource::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+    res.compile()?;
+
+    Ok(())
+}
